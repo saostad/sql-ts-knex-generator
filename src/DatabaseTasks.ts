ebe0ae81bf44684650f3dd8db28bf5de015f7a29
Saeid Ostad <SOstad@kajimausa.com>
2020-01-16 17:20:53 -0500

    added eslint

--- src/DatabaseTasks.ts
+++ src/DatabaseTasks.ts
@@ -1,32 +1,37 @@
-import { Config, Database, Table, DecoratedDatabase } from './Typings';
-import * as TableTasks from './TableTasks';
-import * as handlebars from 'handlebars'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as ColumnTasks from './ColumnTasks';
+import { Config, Database, Table, DecoratedDatabase } from "./Typings";
+import * as TableTasks from "./TableTasks";
+import * as handlebars from "handlebars";
+import * as fs from "fs";
+import * as path from "path";
+import * as ColumnTasks from "./ColumnTasks";
 
 /**
  * Converts a Database definition to TypeScript.
- * 
+ *
  * @export
  * @param {Database} database The Database definition.
  * @param {Config} config The configuration to use.
  * @returns A TypeScript definition, optionally wrapped in a namespace.
  */
-export function stringifyDatabase (database: Database, config: Config): string {
-  let template = fs.readFileSync(path.join(__dirname, './template.handlebars'), 'utf-8')
+export function stringifyDatabase(database: Database, config: Config): string {
+  let template = fs.readFileSync(
+    path.join(__dirname, "./template.handlebars"),
+    "utf-8"
+  );
   if (config.template !== undefined)
-    template = fs.readFileSync(config.template, 'utf-8')
-  const compiler = handlebars.compile(template)
-  database.tables.sort((tableA, tableB) => tableA.name.localeCompare(tableB.name));
-  const grouped : {[key:string]: Table[]} = {}
-  for (let t of database.tables) {
+    template = fs.readFileSync(config.template, "utf-8");
+  const compiler = handlebars.compile(template);
+  database.tables.sort((tableA, tableB) =>
+    tableA.name.localeCompare(tableB.name)
+  );
+  const grouped: { [key: string]: Table[] } = {};
+  for (const t of database.tables) {
     if (grouped[t.schema] === undefined) {
-      grouped[t.schema] = []
+      grouped[t.schema] = [];
     }
-    grouped[t.schema].push(t)
+    grouped[t.schema].push(t);
   }
-  return compiler({ grouped, tables: database.tables, config })
+  return compiler({ grouped, tables: database.tables, config });
 }
 
 /**
@@ -37,7 +42,10 @@ export function stringifyDatabase (database: Database, config: Config): string {
  * @param {Config} config The configuration to use.
  * @returns The decorated database definition.
  */
-export function decorateDatabase(database: Database, config: Config): DecoratedDatabase {
+export function decorateDatabase(
+  database: Database,
+  config: Config
+): DecoratedDatabase {
   return {
     tables: database.tables.map(t => {
       return {
@@ -46,11 +54,17 @@ export function decorateDatabase(database: Database, config: Config): DecoratedD
         columns: t.columns.map(c => {
           return {
             ...c,
-            propertyName: c.name.replace(/ /g,''),
-            propertyType: ColumnTasks.convertType(t.name, t.schema, c.name, c.type, config)
-          }
+            propertyName: c.name.replace(/ /g, ""),
+            propertyType: ColumnTasks.convertType(
+              t.name,
+              t.schema,
+              c.name,
+              c.type,
+              config
+            )
+          };
         })
-      }
-    })  
-  }
+      };
+    })
+  };
 }
